1cd357166f17e0c6ea3745d59111a10da71ab6fa
Brad Hughes <bhughes@huckleberry.mhc.edu>
2011-04-06 11:43:13 -0400

    Edited lang file

--- lang/en/block_timetracker.php
+++ lang/en/block_timetracker.php
@@ -19,7 +19,7 @@
  * http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 $string['datetimeformat']='%m/%d/%y, %I:%M %p';
-$string['timeformat']='%I:%M %p';
+$string['timeformat']='%I:%M%p';
 
 $string['couldnotclockout'] = 'Error adding clock out'; 
 $string['trackermethod'] = 'Which timetracking method would you like to use?';
