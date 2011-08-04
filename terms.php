00e1b4113865ac66d1b99b2511063e964051e57d
Marty Gilbert <mgilbert@mhc.edu>
2011-08-04 18:38:52 -0400

    Added an icon, and terms functionality

---
+++ terms.php
@@ -0,0 +1,133 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This page will allow the worker to input the date, time, and duration of a workunit.
+ *
+ * @package    Block
+ * @subpackage TimeTracker
+ * @copyright  2011 Marty Gilbert & Brad Hughes
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ */
+
+require_once('../../config.php');
+require_once('timetracker_editterms_form.php');
+
+global $CFG, $COURSE, $USER;
+
+require_login();
+
+$courseid = required_param('id', PARAM_INTEGER);
+
+//error_log("In terms.php and Course is $courseid");
+//error_log("In terms.php line 36 and COURSE is $COURSE->id");
+
+$urlparams['id'] = $courseid;
+$urlparams['userid'] = $USER->id;
+
+$termsURL = new moodle_url($CFG->wwwroot.'/blocks/timetracker/terms.php',$urlparams);
+
+$course = $DB->get_record('course', array('id' => $courseid), '*', MUST_EXIST);
+$PAGE->set_course($course);
+$context = $PAGE->context;
+
+//error_log("In terms.php line 47 and COURSE is $COURSE->id");
+
+$PAGE->set_url($termsURL);
+$PAGE->set_pagelayout('course');
+
+if (!has_capability('block/timetracker:manageworkers', $context)) { //supervisor
+    print_error(get_string('notpermissible','block_timetracker'));
+}
+$canmanage = true;
+
+$index = new moodle_url($CFG->wwwroot.'/blocks/timetracker/index.php', $urlparams);
+
+$strtitle = get_string('terms_title','block_timetracker');
+
+$PAGE->set_title($strtitle);
+
+$timetrackerurl = new moodle_url($CFG->wwwroot.'/blocks/timetracker/index.php',$urlparams);
+
+$index = new moodle_url($CFG->wwwroot.'/blocks/timetracker/index.php', $urlparams);
+
+$PAGE->navbar->add(get_string('blocks'));
+$PAGE->navbar->add(get_string('pluginname','block_timetracker'), $timetrackerurl);
+$PAGE->navbar->add($strtitle);
+
+$mform = new timetracker_editterms_form($context);
+
+if ($mform->is_cancelled()){ //user clicked cancel
+    redirect($index,'');
+} else if ($formdata=$mform->get_data()){
+        print_object($formdata);
+        //add/update items to the DB
+        $term = new stdClass();
+        $term->courseid = $courseid;
+        //error_log("in terms.php and $term->courseid");
+
+        for($i=0; $i<3; $i++){
+            $tn = 'termname'.$i;
+            $mn = 'month'.$i;
+            $d = 'day'.$i;
+            $t = 'term'.$i;
+            $term->name = $formdata->$tn;
+            $term->month = $formdata->$mn;
+            $term->day = $formdata->$d;
+
+            if(isset($formdata->$t)){
+                //error_log('Inserting a term');
+                //do update, not insert
+                $term->id = $formdata->$t;
+                $DB->update_record('block_timetracker_term',$term);
+            } else {
+                //error_log('updating a term');
+                //do insert, not update.
+                unset($term->id);
+                $DB->insert_record('block_timetracker_term', $term);
+            }
+        }
+
+        redirect($index,'');
+} else {
+    //form is shown for the first time
+    echo $OUTPUT->header();
+    $maintabs[] = new tabobject('home', $index, 'Main');
+    $maintabs[] = new tabobject('reports', 
+        new moodle_url($CFG->wwwroot.'/blocks/timetracker/reports.php',$urlparams), 'Reports');
+    $maintabs[] = new tabobject('editunit', 
+        new moodle_url($CFG->wwwroot.'/blocks/timetracker/editunit.php',$urlparams), 'Edit Workunit');
+
+    if($canmanage){
+        $maintabs[] = new tabobject('manage', 
+            new moodle_url($CFG->wwwroot.'/blocks/timetracker/manageworkers.php',$urlparams), 
+            'Manage Workers');
+        $maintabs[] = new tabobject('alerts', 
+            new moodle_url($CFG->wwwroot.'/blocks/timetracker/managealerts.php',$urlparams), 
+            'Alerts');
+        $maintabs[] = new tabobject('terms', 
+            new moodle_url($CFG->wwwroot.'/blocks/timetracker/terms.php',$urlparams), 
+            'Terms');
+    }
+    
+    $tabs = array($maintabs);
+    print_tabs($tabs,'terms');
+
+    $mform->display();
+    echo $OUTPUT->footer();
+}
+
+
---
+++ timetracker_editterms_form.php
@@ -0,0 +1,153 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This form will allow the user to input the date, time, and duration of their workunit. 
+ *
+ * @package    TimeTracker
+ * @copyright  Marty Gilbert & Brad Hughes
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ */
+
+require_once ($CFG->libdir.'/formslib.php');
+require_once ('lib.php');
+
+class timetracker_editterms_form extends moodleform {
+
+    function timetracker_editterms_form($context){
+        $this->context = $context;
+        parent::__construct();
+    }
+
+    function definition() {
+        global $CFG, $DB, $COURSE;
+
+        $mform =& $this->_form; // Don't forget the underscore! 
+
+        $mform->addElement('hidden','id',$COURSE->id);
+
+
+        //check to make sure that if $this->userid != $USER->id that they have
+        //the correct capability TODO
+        if(!has_capability('block/timetracker:manageworkers',$this->context)){
+            print_error('Insufficient permission to edit this workunit');
+            return;
+        }
+
+        $terms = $DB->get_records('block_timetracker_term', 
+            array('courseid'=>$COURSE->id),'month, day');
+
+        $days = array();
+        for($i = 1; $i <= 31; $i++){
+            $days[$i] = $i;
+        }
+
+        $months = array(
+            1 =>'January',
+            2=>'February',
+            3=>'March',
+            4=>'April',
+            5=>'May',
+            6=>'June',
+            7=>'July',
+            8=>'August',
+            9=>'September',
+            10=>'October',
+            11=>'November',
+            12=>'December');
+
+        //default values
+        $termmonth = array();
+        $termday = array();
+
+        //jan 1
+        $termname[0] = 'Spring';
+        $termmonth[0] = 1;
+        $termday[0] = 1;
+        //jun 1
+        $termname[1] = 'Summer';
+        $termmonth[1] = 5;
+        $termday[1] = 15;
+        //aug 1
+        $termname[2] = 'Fall';
+        $termmonth[2] = 8;
+        $termday[2] = 1;
+
+        //Should allow them to add/delete as many terms as necessary
+        if($terms){
+            $counter = 0;
+            foreach ($terms as $term){
+                $mform->addElement('hidden','term'.$counter,$term->id);
+                $termname[$counter]=$term->name;
+                $termmonth[$counter]=$term->month; 
+                $termday[$counter]=$term->day; 
+                $counter++;
+                if($counter > 2) break; //only allow 3 terms -- XXX fix this
+            }
+             
+        }
+
+        //$mform->addElement('header', 'general', get_string('terms_title','block_timetracker')); 
+
+        for($i = 0; $i < 3; $i++){
+            $mform->addElement('header', 'general', 'Term '.($i+1)); 
+
+            //$mform->addElement('text','termname'.$i, 'Term name', $termname[$i]); 
+            $mform->addElement('text','termname'.$i, 'Term name'); 
+            $mform->setDefault('termname'.$i , $termname[$i]);
+            $mform->addRule('termname'.$i, null, 'required', null, 'client', 'false');
+
+            $mform->addElement('select', 'month'.$i, 'Start day', $months);
+            $mform->setDefault('month'.$i, $termmonth[$i]);
+
+            $mform->addElement('select', 'day'.$i, 'Start day', $days);
+            $mform->setDefault('day'.$i, $termday[$i]);
+
+        }
+		
+        $this->add_action_buttons(true,get_string('savebutton','block_timetracker'));
+    }
+
+    function validation ($data){
+        $errors = array();
+        
+        $curryear = date("Y"); 
+        $time1 = mktime(0,0,0,$data['month0'],$data['day0'], $curryear);
+        $time2 = mktime(0,0,0,$data['month1'],$data['day1'], $curryear);
+        $time3 = mktime(0,0,0,$data['month2'],$data['day2'], $curryear);
+
+        if($time2 > $time3)
+            $errors['month2'] = 'Term 3 cannot come before term 2';
+        if($time1 > $time2)
+            $errors['month1'] = 'Term 2 cannot come before term 1';
+
+        $hasthirty = array(2,4,6,9,11); //months with 31
+        for($i=0; $i<3; $i++){
+            if(in_array($data['month'.$i],$hasthirty) && $data['day'.$i] > 30){
+                $errors['month'.$i] = 'Month cannot have more than 30 days';
+                break;
+            }
+            if($data['month'.$i] == 2 && $data['month'.$i] > 28) {
+               $errors['month'.$i] = 'Month cannot have more than 28 days'; 
+               break;
+            }
+        }
+
+        return $errors;
+        
+    }
+}
