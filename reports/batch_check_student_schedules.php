84c5bff5dcfed48a8a2bc8e9c685cc2ecf2fb2c4
Marty Gilbert <mgilbert@mhc.edu>
2011-10-29 16:40:54 -0400

    ok

--- reports/batch_check_student_schedules.php
+++ reports/batch_check_student_schedules.php
@@ -2,8 +2,8 @@
 <?php
 
 define('CLI_SCRIPT', true);
-require_once('../../../config.php');
-require_once('../lib.php');
+require_once('../../config.php');
+require_once('lib.php');
 //require_once($CFG->dirroot . '/backup/util/includes/backup_includes.php');
 
 global $CFG, $DB, $USER;
@@ -19,7 +19,8 @@ global $CFG, $DB, $USER;
 */
 
 //start/stop time for search
-$from=1314849600;
+//$from=1314849600; //Sept 1
+$from=1317441600; //Oct 1
 $to=time();
 $datetimeformat='%m/%d/%y %I:%M %p';
 
@@ -32,7 +33,7 @@ $day_names['r'] = 'Thursday';
 $day_names['f'] = 'Friday';
 $day_names['s'] = 'Saturday';
 
-if(($handle = fopen("2011Fall_student_schedules.csv", "r")) !== FALSE){
+if(($handle = fopen("../2011Fall_student_schedules.csv", "r")) !== FALSE){
     while(($data = fgetcsv($handle, 1000, ",")) !== FALSE){
 
         $email = $data[0].'@mhc.edu';
@@ -81,6 +82,20 @@ if(($handle = fopen("2011Fall_student_schedules.csv", "r")) !== FALSE){
                 }
 
                 $iterator = strtotime("Next $day_names[$day]", $from);
+                if($starttime > 1200){
+                    $dispstarttime = $starttime - 1200;
+                    $dispstarttime .='pm';
+                } else {
+                    $dispstarttime = $startime.'am';
+                }
+
+                if($endtime > 1200){
+                    $dispendtime = $endtime - 1200;
+                    $dispendtime .= 'pm';
+                } else {
+                    $dispendtime = $endtime.'am';
+                }
+
                 while($iterator < $to){
                     //echo userdate($iterator, $datetimeformat)."\n";
                     $tdate = usergetdate($iterator);
@@ -89,9 +104,11 @@ if(($handle = fopen("2011Fall_student_schedules.csv", "r")) !== FALSE){
                     if(strlen($starttime)==4){
                         $tdate['hours'] = substr($starttime, 0, 2);
                         $tdate['minutes'] = substr($starttime, 2, 2);
+                        $tdate['minutes'] = $tdate['minutes'] + 5;
                     } else {
                         $tdate['hours'] = substr($starttime, 0, 1);
                         $tdate['minutes'] = substr($starttime, 1, 2);
+                        $tdate['minutes'] = $tdate['minutes'] + 5;
                     }
 
                     $in = make_timestamp($tdate['year'], $tdate['mon'], $tdate['mday'],
@@ -102,29 +119,35 @@ if(($handle = fopen("2011Fall_student_schedules.csv", "r")) !== FALSE){
                     if(strlen($endtime)==4){
                         $tdate['hours'] = substr($endtime,0,2);
                         $tdate['minutes'] = substr($endtime,2,2);
+                        $tdate['minutes'] = $tdate['minutes'] - 5;
                     } else {
                         $tdate['hours'] = substr($endtime,0,1);
                         $tdate['minutes'] = substr($endtime,1,2);
+                        $tdate['minutes'] = $tdate['minutes'] - 5;
                     }
+
                     $out = make_timestamp($tdate['year'], $tdate['mon'], $tdate['mday'],
                         $tdate['hours'], $tdate['minutes']);
                     //echo "Out: ".userdate($out, $datetimeformat)."\n";
+
 
                     $conflicts = find_conflicts($in, $out, $worker->id, -1,
                         $worker->courseid);
                     if(sizeof($conflicts) > 0){
                         foreach($conflicts as $conflict){
                             echo "$worker->lastname,$worker->firstname,".
-                                "$coursename $days_string $starttime to $endtime,".
-                                userdate($iterator,'%A %m/%d/%y',99,false).",".
+                                "$coursename $days_string $dispstarttime to $dispendtime,".
+                                userdate($iterator,'%m/%d/%y %A',99,false).",".
                                 $conflict->display.','.$course->shortname.','.$supervisor."\n";
                         }
                     }
                     $iterator = $iterator + (7 * 86400);
                 }
             }
-
+            break;
         } 
     }
     fclose($handle);
+} else {
+    error_log("Error opening file");
 }
