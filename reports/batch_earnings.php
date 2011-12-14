39ca02bb01ad9d7d350e2161383848bfded2cb3c
Marty Gilbert <mgilbert@mhc.edu>
2011-12-14 13:08:04 -0500

    Modified it to take a start/end time

--- reports/batch_earnings.php
+++ reports/batch_earnings.php
@@ -7,6 +7,8 @@ require_once('../lib.php');
 require_login();
 
 $catid = required_param('catid', PARAM_INTEGER);
+$start = required_param('start', PARAM_INTEGER);
+$end = required_param('end', PARAM_INTEGER);
 $active = optional_param('active', 0, PARAM_INTEGER);
 
 /**
@@ -26,14 +28,18 @@ if (!has_capability('block/timetracker:manageworkers', $context)) {
     print_error('You do not have permission to run this report.');
 }
 
-$categoryinfo = $DB->get_records('course_categories', array('id'=>$catid));
 
-$filename = date("Y_m_d").'_'.$categoryinfo->name.'_Earnings.csv';
-$header = "Department ,Last Name ,First Name ,Earnings ,Max Term Earnings \n";
+$categoryinfo = $DB->get_record('course_categories', array('id'=>$catid));
+$catname = str_replace(' ','', $categoryinfo->name);
+
+$filename = date("Y_m_d").'_'.$catname.'_Earnings.csv';
 header('Content-type: application/ms-excel');
 header('Content-Disposition: attachment; filename='.$filename);
 
-$headers = "Department,Last Name,First Name,Email,Earnings,Max Term Earnings,Remaining \n";
+//$headers = "Department,Last Name,First Name,Email,Earnings,Max Term Earnings,Remaining \n";
+$headers = 
+    "Department,Last Name,First Name,ID,Budget,Hours,Earnings,Max Term Earnings,Remaining \n";
+
 echo $headers;
 
 $courses = get_courses($catid, 'fullname ASC', 'c.id, c.shortname');
@@ -56,10 +62,15 @@ foreach($courses as $course){
             continue;
         }
     
-        $earnings = get_earnings_this_term($worker->id, $course->id);
+        $earnings = get_earnings($worker->id, $course->id, $start, $end);
+        $hours = get_hours_this_period($worker->id, $course->id, $start, $end);
+
         $remaining = $worker->maxtermearnings - $earnings; 
-        $contents = "$course->shortname,$worker->lastname,$worker->firstname,$worker->email,"
-            ."$earnings,$worker->maxtermearnings,$remaining \n";
+        if($remaining < 0) $remaining = 0;
+
+        $contents =
+        "$course->shortname,$worker->lastname,$worker->firstname,$worker->idnum,"
+            ."$worker->budget,$hours,$earnings,$worker->maxtermearnings,$remaining \n";
     
         //Export Data to Simple XLS file
         echo $contents;
