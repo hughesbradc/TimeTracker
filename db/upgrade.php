2cc659d29c4a28a016897f61ade864ed2ce99347
Marty Gilbert <mgilbert@mhc.edu>
2011-12-21 10:30:35 -0500

    Fixing DB with new tables, fields for handling transactions

--- db/upgrade.php
+++ db/upgrade.php
@@ -34,43 +34,31 @@ function xmldb_block_timetracker_upgrade($oldversion) {
     $dbman = $DB->get_manager();
     if ($oldversion < 2011120601) {
 
-        // Define field canedit to be added to block_timetracker_workunit
-        $table = new xmldb_table('block_timetracker_workunit');
-        $field = new xmldb_field('canedit', XMLDB_TYPE_INTEGER, 
-            '1', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '1', 'lasteditedby');
-
-        // Conditionally launch add field canedit
-        if (!$dbman->field_exists($table, $field)) {
-            $dbman->add_field($table, $field);
-        } 
-
-        upgrade_block_savepoint(true, 2011120601, 'timetracker');
 
     }            
 
-    if ($oldversion < 2011120601) {
-          // Define table block_timetracker_timesheet to be created
+    if ($oldversion < 2011121901) {
+
+         // Define table block_timetracker_timesheet to be created
         $table = new xmldb_table('block_timetracker_timesheet');
 
         // Adding fields to table block_timetracker_timesheet
-        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', 
-            XMLDB_UNSIGNED, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
-        $table->add_field('userid', XMLDB_TYPE_INTEGER, 
-            '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null);
-        $table->add_field('courseid', XMLDB_TYPE_INTEGER, '10', 
-            XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null);
-        $table->add_field('repstart', XMLDB_TYPE_INTEGER, '10', 
-            XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null);
-        $table->add_field('repend', XMLDB_TYPE_INTEGER, '10', 
-            XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null);
-        $table->add_field('submitted', XMLDB_TYPE_INTEGER, '10', 
-            XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null);
-        $table->add_field('workersignature', XMLDB_TYPE_INTEGER, '10', 
-            XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0');
-        $table->add_field('supervisorsignature', XMLDB_TYPE_INTEGER, '10', 
-            XMLDB_UNSIGNED, XMLDB_NOTNULL, null, '0');
-        $table->add_field('supermdlid', XMLDB_TYPE_INTEGER, '10', 
-            XMLDB_UNSIGNED, null, null, null);
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL,
+            XMLDB_SEQUENCE, null);
+        $table->add_field('userid', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED,
+            XMLDB_NOTNULL, null, null);
+        $table->add_field('courseid', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED,
+            XMLDB_NOTNULL, null, null);
+        $table->add_field('submitted', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED,
+            XMLDB_NOTNULL, null, null);
+        $table->add_field('workersignature', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED,
+            XMLDB_NOTNULL, null, '0');
+        $table->add_field('supervisorsignature', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED,
+            XMLDB_NOTNULL, null, '0');
+        $table->add_field('supermdlid', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, null,
+            null, null);
+        $table->add_field('transactionid', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED,
+            XMLDB_NOTNULL, null, '0');
 
         // Adding keys to table block_timetracker_timesheet
         $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
@@ -78,46 +66,63 @@ function xmldb_block_timetracker_upgrade($oldversion) {
         // Conditionally launch create table for block_timetracker_timesheet
         if (!$dbman->table_exists($table)) {
             $dbman->create_table($table);
-        }
+        } 
 
-        upgrade_block_savepoint(true, 2011120601, 'timetracker');
+
+        upgrade_block_savepoint(true, 2011121901, 'timetracker');
 
     }            
 
-    if ($oldversion < 2011120601) {
+    if ($oldversion < 2011121901) {
 
-        // Define table block_timetracker_tsunits to be created
-        $table = new xmldb_table('block_timetracker_tsunits');
 
-        // Adding fields to table block_timetracker_tsunits
-        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, 
-            XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
-        $table->add_field('userid', XMLDB_TYPE_INTEGER, '10', 
-            XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null);
-        $table->add_field('timein', XMLDB_TYPE_INTEGER, '10', 
-            XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null);
-        $table->add_field('timeout', XMLDB_TYPE_INTEGER, '10', 
-            XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null);
-        $table->add_field('payrate', XMLDB_TYPE_NUMBER, '10, 2', null, 
+         // Define field canedit to be added to block_timetracker_workunit
+        $table = new xmldb_table('block_timetracker_workunit');
+        $field = new xmldb_field('canedit', XMLDB_TYPE_INTEGER, '1', XMLDB_UNSIGNED, null,
+            null, '1', 'lasteditedby');
+
+        // Conditionally launch add field canedit
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+
+         // Define field timesheetid to be added to block_timetracker_workunit
+        $table = new xmldb_table('block_timetracker_workunit');
+        $field = new xmldb_field('timesheetid', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED,
+            null, null, '0', 'canedit');
+
+        // Conditionally launch add field timesheetid
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+          // Define table block_timetracker_transactn to be created
+        $table = new xmldb_table('block_timetracker_transactn');
+
+        // Adding fields to table block_timetracker_transactn
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL,
+            XMLDB_SEQUENCE, null);
+        $table->add_field('submitted', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, null,
+            null, null);
+        $table->add_field('description', XMLDB_TYPE_TEXT, 'small', null, null, null,
+            null);
+        $table->add_field('mdluserid', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED,
             XMLDB_NOTNULL, null, null);
-        $table->add_field('lastedited', XMLDB_TYPE_INTEGER, '10', 
-            XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null);
-        $table->add_field('lasteditedby', XMLDB_TYPE_INTEGER, '10',     
-            XMLDB_UNSIGNED, XMLDB_NOTNULL, null, null);
+        $table->add_field('categoryid', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, null,
+            null, '0');
 
-        // Adding keys to table block_timetracker_tsunits
+        // Adding keys to table block_timetracker_transactn
         $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
 
-        // Conditionally launch create table for block_timetracker_tsunits
+        // Conditionally launch create table for block_timetracker_transactn
         if (!$dbman->table_exists($table)) {
             $dbman->create_table($table);
         }
 
-        // timetracker savepoint reached
-        upgrade_block_savepoint(true, 2011120601, 'timetracker');
+        upgrade_block_savepoint(true, 2011121901, 'timetracker');
 
     }            
-
 
     return true;
 }
