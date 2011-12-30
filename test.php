ef1b5e903efd128ed0d05fb997af200e8004fc25
Marty Gilbert <mgilbert@mhc.edu>
2011-12-30 13:49:10 -0500

    Demonstrated displaying the calendar of work units.

--- test.php
+++ test.php
@@ -12,9 +12,7 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
+// You should have received a copy of the GNU General Public License // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.  
 /**
  * This block will display a summary of hours and earnings for the worker.
  *
@@ -26,6 +24,7 @@
 require_once(dirname(__FILE__) . '/../../config.php');
 require_once($CFG->libdir . '/tablelib.php');
 require_once('lib.php');
+require_once('timesheet_pdf.php');
 
 require_login();
 
@@ -57,60 +56,74 @@ if (has_capability('block/timetracker:manageworkers', $context)) { //supervisor
 
 echo $OUTPUT->header();
 
-$checkin = 1288084624 + (60 * 60 * 24 * 138)-(60*79);
-$checkout = $checkin + (60 * 60 * 24 * 8)-(60*60*17);
-//$checkout = time();
+echo '
+<style type="text/css">
 
-echo 'Check in:  '. userdate($checkin,
-    get_string('datetimeformat','block_timetracker')).'<br />';
-echo 'Checkout: '. userdate($checkout,
-    get_string('datetimeformat','block_timetracker')).'<br />';
-echo '<br />';
-
-$endofday = (86400+(usergetmidnight($checkin)-1));
-$currcheckin = $checkin;
-
-if(date("H",$endofday) == 22){
-    $endofday += 60 * 60;
-} else if (date("H",$endofday) == 0){
-    $endofday -= 60 * 60;
+table{
+    width: 80%;
+    height: 80%;
+}
+.calendar{
+    border-left: 1px solid black;
+    border-bottom: 1px solid black;
+    border-top: 1px solid black;
+}
+table,div,td,th,tr{
+    font-weight: normal;
+    font-size: 18px;
+    font-family: helvetica;
 }
 
-while ($currcheckin < $checkout){
-
-    //add to $DB
-    $output = userdate($currcheckin, get_string('datetimeformat','block_timetracker'));
-    $output .= ' to '.userdate($endofday,
-        get_string('datetimeformat','block_timetracker'));
-    $output .= ', for '.format_elapsed_time($endofday - $currcheckin);
-    $output .= '<br />';
-    //don't echo $output, but add to DB
-    echo $output;
-
-    //update checkin and checkout
-    //update to midnight
-    //$currcheckin += $tomidnight + 1;
-    $currcheckin = $endofday + 1;
-
-    //find next 23:59:59
-    $endofday = 86400 + (usergetmidnight($currcheckin)-1);
-
-    //because I can't get dst_offset_on to work!
-    $usersdate = usergetdate($endofday);
-    if($usersdate['hours'] == 22){ 
-        $endofday += 60 * 60;
-    } else if ($usersdate['hours'] == 0){
-        $endofday -= 60 * 60;
-    }
-
-    //if not a full day, don't go to 23:59:59 
-    //but rather checkout time
-    if($endofday > $checkout){
-        error_log("not a full day");
-        $endofday = $currcheckin + ($checkout - $currcheckin);
-    } 
-    //break;
-
+table{
+   padding: 0;
+   spacing: 0;
+   border: 1px solid black;
+   border-collapse: separate;
+   margin-left: auto;
+   margin-right: auto;
 }
+
+span.thirteen{
+    font-weight: bold;
+    font-size: 23px;
+    font-family: helvetica;
+}
+
+span.ten{
+    font-weight: bold;
+    font-size: 20px;
+    font-family: helvetica;
+}
+
+span.eight{
+    font-weight: bold;
+    font-size: 18px;
+    font-family: helvetica;
+}
+
+span.seven{
+    font-weight: bold;
+    font-size: 17px;
+    font-family: helvetica;
+}
+
+</style>
+';
+
+$pages = generate_html(1320120000, time(), 4, 112, 'I','',1);
+
+foreach($pages as $page){
+    $page = str_replace('<font size="13">', '<span class="thirteen">',$page);
+    $page = str_replace('<font size="10">', '<span class="ten">',$page);
+    $page = str_replace('<font size="8">', '<span class="eight">',$page);
+    $page = str_replace('<font size="7">', '<span class="seven">',$page);
+    $page = str_replace('</font>', '</span>',$page);
+    $page = str_replace('<hr style="height: 1px" />','', $page);
+    echo $page;
+    echo "\n\n\n";
+}
+
+//echo '</div>';
+
 
 echo $OUTPUT->footer();
